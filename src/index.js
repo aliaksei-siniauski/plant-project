ec6bca9297aa5780252748c49ef14bc85a50c88a
github-username <mail@gmail.com>
2022-11-09 00:22:34 +0100

    fix: change blur function import path

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import { pollyfillForForEach, addDropDownClickHandler } from "./js/DropDown.js";
-import { addTagClickHandler } from "./js/Blur.js";
+import { addButtonClickHandler } from "./js/Blur.js";
